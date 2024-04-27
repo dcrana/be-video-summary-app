0e897241697c666de7529878c425aa07d32380d1
SUNIL SALAT <sunilsalat@yahoo.com>
2024-04-27 23:39:46 +0530

    home page completed , global api started

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,18 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface ComponentsFeature extends Schema.Component {
+  collectionName: 'components_components_features';
+  info: {
+    displayName: 'Feature';
+    description: '';
+  };
+  attributes: {
+    heading: Attribute.String;
+    subHeading: Attribute.Text;
+    icon: Attribute.Enumeration<['CLOCK_ICON', 'CHECK_ICON', 'CLOUD_ICON']>;
+  };
+}
+
 export interface ComponentsLink extends Schema.Component {
   collectionName: 'components_components_links';
   info: {
@@ -13,6 +26,44 @@ export interface ComponentsLink extends Schema.Component {
   };
 }
 
+export interface LayoutFeatureSection extends Schema.Component {
+  collectionName: 'components_layout_feature_sections';
+  info: {
+    displayName: 'Feature Section';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String;
+    description: Attribute.Text;
+    feature: Attribute.Component<'components.feature', true>;
+  };
+}
+
+export interface LayoutFooter extends Schema.Component {
+  collectionName: 'components_layout_footers';
+  info: {
+    displayName: 'Footer';
+    description: '';
+  };
+  attributes: {
+    logoText: Attribute.Component<'components.link'>;
+    text: Attribute.Text;
+    socialLinks: Attribute.Component<'components.link', true>;
+  };
+}
+
+export interface LayoutHeader extends Schema.Component {
+  collectionName: 'components_layout_headers';
+  info: {
+    displayName: 'Header';
+    description: '';
+  };
+  attributes: {
+    logoText: Attribute.Component<'components.link'>;
+    ctaButton: Attribute.Component<'components.link'>;
+  };
+}
+
 export interface LayoutHeroSection extends Schema.Component {
   collectionName: 'components_layout_hero_sections';
   info: {
@@ -21,16 +72,20 @@ export interface LayoutHeroSection extends Schema.Component {
   };
   attributes: {
     heading: Attribute.String;
-    subHeading: Attribute.Text;
     image: Attribute.Media;
     link: Attribute.Component<'components.link'>;
+    subHeading: Attribute.Text;
   };
 }
 
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
+      'components.feature': ComponentsFeature;
       'components.link': ComponentsLink;
+      'layout.feature-section': LayoutFeatureSection;
+      'layout.footer': LayoutFooter;
+      'layout.header': LayoutHeader;
       'layout.hero-section': LayoutHeroSection;
     }
   }
